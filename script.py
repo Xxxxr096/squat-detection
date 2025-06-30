bb4078201081adf9d042b5eacc8482e155f7339e
Amine CHABANE <amine.chabane006@gmail.com>
2025-06-30 16:09:09 +0200

    ajout

--- script.py
+++ script.py
@@ -3,6 +3,9 @@ import numpy as np
 import mediapipe as mp
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
+import sys
+
+print(sys.version)
 
 mp_pose = mp.solutions.pose
 pose = mp_pose.Pose()
